0550d43a94737658fd1faf984764462047a2b06c
JRamonCarralero <grukduz@gmail.com>
2025-07-30 20:22:12 +0200

    feat: :sparkles: Creating an authentication module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,12 +4,16 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI!),
     ProductModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
