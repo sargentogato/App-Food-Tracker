d8105e4f8aa49a158a958d4d0b624d6ede6ee309
JRamonCarralero <grukduz@gmail.com>
2025-07-30 21:06:30 +0200

    feat: :sparkles: user schema

---
+++ backend/src/users/schemas/user.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop({ required: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  role: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
