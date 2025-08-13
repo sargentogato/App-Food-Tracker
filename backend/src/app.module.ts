7b5bb258e4ac5c395df44208c0c94135892b2e56
Marta <vilaseca.marta@gmail.com>
2025-08-13 19:46:17 +0200

    refactor: :zap: Remove dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ProductModule } from './product/product.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { UserModule } from './user/user.module';
 import { User } from './user/entities/user.entity';
+import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI!),
-    ProductModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.MYSQL_HOST,
@@ -26,6 +23,7 @@ import { User } from './user/entities/user.entity';
       autoLoadEntities: true,
     }),
     UserModule,
+    ItemsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,26 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { InjectConnection } from '@nestjs/mongoose';
-import { Connection, ConnectionStates } from 'mongoose';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
-  }
-
-  constructor(@InjectConnection() private readonly connection: Connection) {}
-
-  onModuleInit(): void {
-    try {
-      const status = this.connection.readyState;
-      // Import ConnectionStates from mongoose and compare with ConnectionStates.connected
-      console.log(
-        status === ConnectionStates.connected
-          ? 'Database is connected'
-          : 'Database is not connected',
-      );
-    } catch (error) {
-      console.log('Database connection error');
-    }
   }
 }
