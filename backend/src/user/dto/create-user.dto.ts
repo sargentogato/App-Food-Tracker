d71842045b50c351264d39f068e42704a20167bf
Marta <vilaseca.marta@gmail.com>
2025-08-13 20:43:18 +0200

    feat: :zap: Update user dto

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { IsEnum, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  username: string;
+
+  @IsString()
+  full_name: string;
+
+  @IsString()
+  email: string;
+
+  @IsEnum(['user', 'admin', 'superadmin'])
+  @IsOptional()
+  role?: string;
+}
