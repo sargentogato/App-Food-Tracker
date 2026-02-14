1262a1022ba87e0db3a63e8fcd607fb0640d7c84
JRamonCarralero <grukduz@gmail.com>
2026-02-14 20:16:39 +0100

    feat: :sparkles: create, update, delete products service

--- backend/src/modules/products/products.controller.ts
+++ backend/src/modules/products/products.controller.ts
@@ -2,14 +2,15 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { GetUser } from 'src/core/decorators/get-user.decorator';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  create(@Body() createProductDto: CreateProductDto, @GetUser('id') userId: number) {
+    return this.productsService.create(createProductDto, userId);
   }
 
   @Get()
@@ -23,8 +24,12 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(+id, updateProductDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+    @GetUser('id') userId: number,
+  ) {
+    return this.productsService.update(+id, updateProductDto, userId);
   }
 
   @Delete(':id')
--- backend/src/modules/products/products.service.ts
+++ backend/src/modules/products/products.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { DeleteResult, Repository } from 'typeorm';
+import { handleDbError } from 'src/core/utils/mysql-error-handler';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto, userId: number) {
+    try {
+      return await this.productsRepository.save({
+        ...createProductDto,
+        created_by: userId,
+        updated_by: userId,
+      });
+    } catch (error: unknown) {
+      return handleDbError(error, 'create product');
+    }
   }
 
   findAll() {
@@ -16,11 +33,32 @@ export class ProductsService {
     return `This action returns a #${id} product`;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto, userId: number) {
+    try {
+      const updateProduct = { ...updateProductDto, updated_by: userId };
+
+      const result = await this.productsRepository.update(id, updateProduct);
+
+      if (result.affected === 0) {
+        throw new NotFoundException(`Product with id ${id} not found`);
+      }
+      return this.findOne(id);
+    } catch (error: unknown) {
+      if (error instanceof NotFoundException) throw error;
+      return handleDbError(error, `update product with id ${id}`);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number): Promise<DeleteResult> {
+    try {
+      const result = await this.productsRepository.delete(id);
+      if (result.affected === 0) {
+        throw new NotFoundException(`Product with id ${id} not found`);
+      }
+      return result;
+    } catch (error: unknown) {
+      if (error instanceof NotFoundException) throw error;
+      return handleDbError(error, `delete product with id ${id}`);
+    }
   }
 }
